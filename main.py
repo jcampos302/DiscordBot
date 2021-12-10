0d9efe4d8c2cf1d0bbfd4cba834c238060bf9d25
jcampos302 <59896858+jcampos302@users.noreply.github.com>
2021-12-09 18:51:32 -0600

    Improved the welcome message

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ intents.members = True
 client = discord.Client(intents=intents)
 
 
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
+    member_count = len(client.users)
+    print(member_count)
+
 
 
 @client.event
@@ -22,6 +26,12 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to the Lewis\'s Cyber Defense Club server!'
     )
 
+    channel = client.get_channel(872376054612906014)
+    member_count = len(client.users)
+    await channel.send('Hi there {}, welcome to Cyber Defense Club! Please read our #rules and check out '
+                       'our #roles section to become a member. If you need any assistance please ask for an '
+                       'admin. ({} members!)'.format(member.mention, member_count))
+
 
 @client.event
 async def on_error(event, *args, **kwargs):
@@ -30,6 +40,7 @@ async def on_error(event, *args, **kwargs):
             f.write(f'Unhandled message: {args[0]}\n')
         else:
             raise
+
 
 token = os.getenv('Token')
 client.run(token)
