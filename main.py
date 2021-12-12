cf09b6d0e5abd4a343ff2965aa6b3d646929f8d9
jcampos302 <59896858+jcampos302@users.noreply.github.com>
2021-12-11 21:04:46 -0600

    Added roles
    Added roles based off reaction, will soon implement the opposite for when they remove reaction

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 from dotenv import load_dotenv
 import discord
-
+from discord.utils import get
 
 load_dotenv()
 intents = discord.Intents.default()
 intents.members = True
+intents.reactions = True
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -38,10 +40,74 @@ async def on_message(message):
 
     if message.content[0] == '$':
         print(f'Attempting Command: {message.content}')
+    if message.content == '$help':
+        await message     .channel.send('Here are the following commands {}:\n\n$help -Displays Commands'
+                                   '\n$ping -Ping the bot\n\n More to come!'.format(message.author.mention))
     if message.content == '$ping':
         await message.channel.send('Pinging {}'.format(message.author.mention))
     if message.content == '$mute':
         print(f"Work in Progress")
+
+
+@client.event
+async def on_raw_reaction_add(payload):
+    channel = 882791223960682517
+    if payload.channel_id != channel:
+        return
+    else:
+        if str(payload.emoji) == '👾':
+            role = 'Member'
+            member = payload.member
+            await member.add_roles(get(member.guild.roles, name=role))
+            print(f'[+] Adding {member.nick} to member role!')
+            await member.create_dm()
+            await member.dm_channel.send(
+                f'Hi {member.name}, thanks for becoming a member!'
+            )
+        if str(payload.emoji) == '<:hackerman:882796534272516136>':
+            role = 'Red Team'
+            member = payload.member
+            await member.add_roles(get(member.guild.roles, name=role))
+            print(f'[+] Adding {member.nick} to red team role!')
+        if str(payload.emoji) == '🛡️':
+            role = 'Blue Team'
+            member = payload.member
+            await member.add_roles(get(member.guild.roles, name=role))
+            print(f'[+] Adding {member.nick} to blue team role!')
+        if str(payload.emoji) == '📚':
+            role = 'Law and Governance'
+            member = payload.member
+            await member.add_roles(get(member.guild.roles, name=role))
+            print(f'[+] Adding {member.nick} to Law and Governance role!')
+        if str(payload.emoji) == '<:iusearchbtw:882799034077704192>':
+            role = 'Architecture'
+            member = payload.member
+            await member.add_roles(get(member.guild.roles, name=role))
+            print(f'[+] Adding {member.nick} to Architecture role!')
+        if str(payload.emoji) == '⌨':
+            role = 'Software'
+            member = payload.member
+            await member.add_roles(get(member.guild.roles, name=role))
+            print(f'[+] Adding {member.nick} to Software role!')
+        if str(payload.emoji) == '🐸':
+            role = 'Management'
+            member = payload.member
+            await member.add_roles(get(member.guild.roles, name=role))
+            print(f'[+] Adding {member.nick} to Management role!')
+        if str(payload.emoji) == '👴':
+            role = 'Alumni'
+            member = payload.member
+            await member.add_roles(get(member.guild.roles, name=role))
+            print(f'[+] Adding {member.nick} to Alumni role!')
+            await member.create_dm()
+            await member.dm_channel.send(
+                f'Hi {member.name}, thank you and welcome alumni!'
+            )
+        if str(payload.emoji) == '🦩':
+            role = 'Order of The Purple Flamingo'
+            member = payload.member
+            await member.add_roles(get(member.guild.roles, name=role))
+            print(f'[+] Adding {member.nick} to the Order of the Purple Flamingo!')
 
 
 @client.event
